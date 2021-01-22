42efbe4781c254c83279cb81f7240c2926389276
pedro742k2 <pmpb742k2@gmail.com>
2021-01-22 10:15:39 +0000

    ...

--- Controllers/GetData.js
+++ Controllers/GetData.js
@@ -4,12 +4,12 @@ const handleGetData = (db) => (req, res) => {
   const hv = ApHv.split("-")[1];
   const { currentDate, measurementType } = req.body;
 
-  console.log(ApHv, currentDate, measurementType);
-
   const date = currentDate.split("-");
   const day = date[0];
   const month = date[1];
   const year = date[2];
+
+  console.log(day, month, year);
 
   const firstDataFromHours = [];
   let hour = 0;
@@ -26,6 +26,7 @@ const handleGetData = (db) => (req, res) => {
       .andWhereRaw("EXTRACT(YEAR FROM readings_date) = ?", [year])
       .orderBy("readings_date")
       .then((data) => {
+        console.log(data);
         data.forEach((value) => {
           const valueHour = new Date(String(value.readings_date)).getHours();
           if (valueHour === hour) {
