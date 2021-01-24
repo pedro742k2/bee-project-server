37125004cbced00e99fdaf6c7cf91d07ec9ea89c
pedro742k2 <pmpb742k2@gmail.com>
2021-01-24 13:03:56 +0000

    Fixing register

--- Controllers/Register.js
+++ Controllers/Register.js
@@ -1,26 +1,22 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
   const { userName, email, password } = req.body;
 
-  bcrypt
-    .hash(password, 10, (error, hash) => {
-      db.insert({
-        user_name: userName,
-        email,
-        password: hash,
-      })
-        .into("users")
-        .then(() => {
-          res.json("User has beed registred");
-          db.commit;
-        })
-        .catch(() => {
-          res.json("Something went wrong");
-          db.rollback;
-        });
+  bcrypt.hash(password, 10, (error, hash) => {
+    db.insert({
+      user_name: userName,
+      email,
+      password: hash,
     })
-    .catch(() => {
-      res.json("Something went wrong");
-    });
+      .into("users")
+      .then(() => {
+        res.json("User has beed registred");
+        db.commit;
+      })
+      .catch(() => {
+        res.json("Something went wrong");
+        db.rollback;
+      });
+  });
 };
 
 module.exports = {
