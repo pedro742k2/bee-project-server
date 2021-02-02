a2d5e5e97c8fd0b19bd46398a126747cd4034346
pedro742k2 <pmpb742k2@gmail.com>
2021-02-02 19:00:03 +0000

    ...

--- Controllers/AddHives.js
+++ Controllers/AddHives.js
@@ -1,112 +1,3 @@
-/* const handleAddHives = (db) => (req, res) => {
-  const { userName, email, ApHv, hiveId, add } = req.body;
-  let valid = true;
-
-  db("hives_info")
-    .select("hive_id")
-    .then((data) => {
-      if (data.length === 0) {
-        db("hives_info")
-          .insert({
-            hive_id: hiveId,
-            apiary_number: ApHv.split("-")[0],
-            hive_number: ApHv.split("-")[1],
-          })
-          .then(db.commit)
-          .catch(db.rollback);
-      } else {
-        db("hives_info")
-          .where("hive_id", hiveId)
-          .update({
-            apiary_number: ApHv.split("-")[0],
-            hive_number: ApHv.split("-")[1],
-          })
-          .then(db.commit)
-          .catch(db.rollback);
-      }
-    })
-    .catch(() => {
-      res.status(400).json("Something went wrong");
-      valid = false;
-      return false;
-    });
-
-  if (ApHv === "-" || ApHv.length < 3) {
-    res.json("Invalid input");
-  } else if (valid) {
-    const sentApHv = ApHv.split("-");
-
-    if (sentApHv.length === 2) {
-      db("users")
-        .where({
-          user_name: userName,
-          email: email,
-        })
-        .select("ap_hv")
-        .then((data) => {
-          let empty = false;
-
-          const ApHvFromServer = data[0].ap_hv;
-
-          if (data.length >= 1) {
-            try {
-              if (add) {
-                if (ApHvFromServer.includes(ApHv)) {
-                  res.status(400).json("Already exists");
-                  return false;
-                }
-              } else {
-                if (!ApHvFromServer.includes(ApHv)) {
-                  res.status(400).json("This hive no longer exists");
-                  return false;
-                }
-              }
-            } catch {
-              if (ApHvFromServer === null) {
-                empty = true;
-                if (!add) {
-                  res.status(400).json("This hive no longer exists");
-                  return false;
-                }
-              }
-              // Continues, there are no hives
-            }
-
-            let newApHvString = undefined;
-
-            if (empty && !add) {
-              res.status(400).json("This hive no longer exists");
-              return false;
-            } else if (empty && add) {
-              newApHvString = ApHv + ";";
-            } else {
-              newApHvString = add
-                ? ApHvFromServer + ApHv + ";"
-                : ApHvFromServer.replace(ApHv + ";", "");
-              // newApHvString = ApHvFromServer + ApHv + ";";
-            }
-
-            db("users")
-              .where({
-                user_name: userName,
-                email: email,
-              })
-              .update("ap_hv", newApHvString)
-              .then(() => {
-                res.json("Successfuly updated");
-              })
-              .catch(() => {
-                res.status(400).json("Something went wrong");
-                return false;
-              });
-          }
-        });
-    } else {
-      res.status(400).json("Invalid input");
-    }
-  }
-}; */
-
 const handleAddHives = (db) => (req, res) => {
   const { userName, email, IdApHv, add } = req.body;
   let valid = true;
@@ -118,35 +9,55 @@ const handleAddHives = (db) => (req, res) => {
   if (IdApHv.length < 5) {
     res.json("Invalid input");
   } else {
-    db("hives_info")
-      .select("hive_id")
-      .where("hive_id", hiveId)
-      .then((data) => {
-        if (data.length === 0) {
-          db("hives_info")
-            .insert({
-              hive_id: hiveId,
-              apiary_number: apiaryNumber,
-              hive_number: hiveNumber,
-            })
-            .then(db.commit)
-            .catch(db.rollback);
-        } else {
-          db("hives_info")
-            .where("hive_id", hiveId)
-            .update({
-              apiary_number: apiaryNumber,
-              hive_number: hiveNumber,
-            })
-            .then(db.commit)
-            .catch(db.rollback);
-        }
-      })
-      .catch(() => {
-        res.status(400).json("Something went wrong");
-        valid = false;
-        return false;
-      });
+    /* Adding apiary and hive number to hive id if add = true */
+    if (add) {
+      db("hives_info")
+        .select("hive_id")
+        .where("hive_id", hiveId)
+        .then((data) => {
+          if (data.length === 0) {
+            db("hives_info")
+              .insert({
+                hive_id: hiveId,
+                apiary_number: apiaryNumber,
+                hive_number: hiveNumber,
+              })
+              .then(db.commit)
+              .catch(db.rollback);
+          } else {
+            db("hives_info")
+              .where("hive_id", hiveId)
+              .update({
+                apiary_number: apiaryNumber,
+                hive_number: hiveNumber,
+              })
+              .then(db.commit)
+              .catch(db.rollback);
+          }
+        })
+        .catch(() => {
+          res.status(400).json("Something went wrong");
+          valid = false;
+          return false;
+        });
+    } else {
+      /* Removing all hive_id (including) info if add = false */
+      db("hives_info")
+        .select("hive_id")
+        .where("hive_id", hiveId)
+        .then((data) => {
+          if (data.length === 0) {
+            res.json("Unable to remove, that hive doesn't exist anymore");
+          } else {
+            db("hives_info").where("hive_id", hiveId).del();
+          }
+        })
+        .catch(() => {
+          res.status(400).json("Something went wrong");
+          valid = false;
+          return false;
+        });
+    }
 
     if (valid) {
       db("users")
@@ -156,124 +67,70 @@ const handleAddHives = (db) => (req, res) => {
           email: email,
         })
         .then((data) => {
-          console.log(data);
           data = data[0].hives_id;
-          console.log(data);
 
           if (data === null) {
-            const newData = hiveId + ";";
-            console.log(newData);
+            if (add) {
+              const newData = hiveId + ";";
+              console.log(newData);
 
-            db("users")
-              .where({
-                user_name: userName,
-                email: email,
-              })
-              .update("hives_id", newData)
-              .then(() => {
-                res.json("Successfuly updated");
-              })
-              .catch((error) => {
-                console.log(error);
-                res.status(400).json("Something went wrong");
-              });
+              db("users")
+                .where({
+                  user_name: userName,
+                  email: email,
+                })
+                .update("hives_id", newData)
+                .then(() => {
+                  res.json("Successfuly updated");
+                })
+                .catch((error) => {
+                  console.log(error);
+                  res.status(400).json("Something went wrong");
+                });
+            } else {
+              res.json("Unable to remove, that hive doesn't exist anymore");
+            }
           } else if (data.includes(hiveId)) {
-            res.json("This hive id already exists in your account");
+            if (add) {
+              res.json("This hive id already exists in your account");
+            } else {
+              const newData = data.replace(hiveId + ";", "");
+              db("users")
+                .where({
+                  user_name: userName,
+                  email: email,
+                })
+                .update("hives_id", newData)
+                .then(() => {
+                  res.json("Successfuly removed");
+                })
+                .catch(() => {
+                  res.status(400).json("Something went wrong");
+                });
+            }
           } else {
-            const newData = data + hiveId + ";";
+            if (add) {
+              const newData = data + hiveId + ";";
 
-            db("users")
-              .where({
-                user_name: userName,
-                email: email,
-              })
-              .update("hives_id", newData)
-              .then(() => {
-                res.json("Successfuly updated");
-              })
-              .catch((error) => {
-                console.log(error);
-                res.status(400).json("Something went wrong");
-              });
+              db("users")
+                .where({
+                  user_name: userName,
+                  email: email,
+                })
+                .update("hives_id", newData)
+                .then(() => {
+                  res.json("Successfuly added");
+                })
+                .catch(() => {
+                  res.status(400).json("Something went wrong");
+                });
+            } else {
+              res.status(400).json("Something went wrong");
+            }
           }
         });
     }
   }
-
-  /* if (ApHv === "-" || ApHv.length < 3) {
-    res.json("Invalid input");
-  } else if (valid) {
-    const sentApHv = ApHv.split("-");
-
-    if (sentApHv.length === 2) {
-      db("users")
-        .where({
-          user_name: userName,
-          email: email,
-        })
-        .select("ap_hv")
-        .then((data) => {
-          let empty = false;
-
-          const ApHvFromServer = data[0].ap_hv;
-
-          if (data.length >= 1) {
-            try {
-              if (add) {
-                if (ApHvFromServer.includes(ApHv)) {
-                  res.status(400).json("Already exists");
-                  return false;
-                }
-              } else {
-                if (!ApHvFromServer.includes(ApHv)) {
-                  res.status(400).json("This hive no longer exists");
-                  return false;
-                }
-              }
-            } catch {
-              if (ApHvFromServer === null) {
-                empty = true;
-                if (!add) {
-                  res.status(400).json("This hive no longer exists");
-                  return false;
-                }
-              }
-              // Continues, there are no hives
-            }
-
-            let newApHvString = undefined;
-
-            if (empty && !add) {
-              res.status(400).json("This hive no longer exists");
-              return false;
-            } else if (empty && add) {
-              newApHvString = ApHv + ";";
-            } else {
-              newApHvString = add
-                ? ApHvFromServer + ApHv + ";"
-                : ApHvFromServer.replace(ApHv + ";", "");
-              // newApHvString = ApHvFromServer + ApHv + ";";
-            }
-
-            db("users")
-              .where({
-                user_name: userName,
-                email: email,
-              })
-              .update("ap_hv", newApHvString)
-              .then(() => {
-                res.json("Successfuly updated");
-              })
-              .catch(() => {
-                res.status(400).json("Something went wrong");
-                return false;
-              });
-          }
-        });
-    } else {
-      res.status(400).json("Invalid input");
-    } 
-  } */
 };
 
 module.exports = {
