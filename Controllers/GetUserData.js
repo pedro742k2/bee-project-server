bb0c8211dc41c773c6a09b06a8115ef8098bf7e6
pedro742k2 <pmpb742k2@gmail.com>
2021-02-02 19:56:11 +0000

    ...

--- Controllers/GetUserData.js
+++ Controllers/GetUserData.js
@@ -1,11 +1,26 @@
 const handleGetUsersData = (db) => (req, res) => {
-  const { userName, email } = req.body;
+  const { userName, email, id, getHivesId } = req.body;
 
   db("users")
-    .select("name", "hives_id")
+    .select("hives_id")
     .where({ user_name: userName, email: email })
     .then((hivesId) => {
-      res.json(hivesId);
+      if (getHivesId) {
+        res.json(hivesId);
+      } else {
+        hivesId = hivesId[0].hives_id;
+        if (hivesId.includes(id)) {
+          db("hives_info")
+            .select("*")
+            .where("hive_id", id)
+            .then((data) => {
+              res.json(data);
+            })
+            .catch(() => {
+              res.status(400).json("error");
+            });
+        }
+      }
     })
     .catch(() => {
       res.status(400).json("Unable to consult the database");
