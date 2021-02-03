17bb3e0aa28e111ffb7cc5db278e9ccdae734428
pedro742k2 <pmpb742k2@gmail.com>
2021-02-03 10:25:03 +0000

    ...

--- Controllers/DataFromSensor.js
+++ Controllers/DataFromSensor.js
@@ -23,16 +23,22 @@ const handleDataFromSensor = (db) => (req, res) => {
         const weight = data.split("-")[2];
         const battery = data.split("-")[3];
 
-        db("apiaries").insert({
-          hive_id: hiveId,
-          temperature: temp,
-          humidity: hmdt,
-          weight,
-          battery,
-          readings_date,
-        });
-        /* .then(db.commit)
-          .catch(db.rollback); */
+        db("apiaries")
+          .insert({
+            hive_id: hiveId,
+            temperature: temp,
+            humidity: hmdt,
+            weight,
+            battery,
+            readings_date,
+          })
+          .then((data) => {
+            console.log(data);
+            db.commit;
+          })
+          .catch(() => {
+            db.rollback;
+          });
 
         res.json({
           stored: true,
