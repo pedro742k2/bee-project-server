7d3c9e8182df270a17ccc191e970d046e4bc4e05
pedro742k2 <pmpb742k2@gmail.com>
2021-02-07 20:39:37 +0000

    ...

--- Controllers/ChangeUserInfo.js
+++ Controllers/ChangeUserInfo.js
@@ -4,22 +4,13 @@ const handleChangeUserInfo = (db) => (req, res) => {
   db("users")
     .where({ user_name: userName, email: email })
     .update(field, value)
-    .then(() => {
-      res.json("Updated successfully");
+    .returning("user_name", "email", "name", "hives_id")
+    .then((data) => {
+      res.json("Updated successfully " + data);
     })
     .catch(() => {
       res.status(400).json("Error");
     });
-
-  /* db("users")
-    .where({ user_name: userName, email: email })
-    .update("name", name)
-    .then(() => {
-      res.json("Updated successfully");
-    })
-    .catch(() => {
-      res.status(400).json("Error");
-    }); */
 };
 
 module.exports = {
