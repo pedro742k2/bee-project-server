0a771464673942d7c953334b59289a06438a2655
pedro742k2 <pmpb742k2@gmail.com>
2021-02-20 16:26:16 +0000

    Added hourly measurements

--- Controllers/GetData.js
+++ Controllers/GetData.js
@@ -9,89 +9,118 @@ const handleGetData = (db) => (req, res) => {
   const firstDataFromHours = [];
   let hour = 0;
 
-  if (measurementType === "daily") {
-    db.select(
-      "external_temperature",
-      "internal_temperature",
-      "humidity",
-      "weight",
-      "battery",
-      "readings_date"
-    )
-      .orderBy("readings_date")
-      .from("apiaries")
-      .where({
-        hive_id: hiveId,
-      })
-      .whereRaw("EXTRACT(DAY FROM readings_date) = ?", [day])
-      .andWhereRaw("EXTRACT(MONTH FROM readings_date) = ?", [month])
-      .andWhereRaw("EXTRACT(YEAR FROM readings_date) = ?", [year])
-      .then((data) => {
-        console.log(data);
-        data.forEach((value) => {
-          const valueHour = new Date(value.readings_date).getHours();
-          if (valueHour === hour) {
-            firstDataFromHours.push(value);
-            hour++;
-          } else {
-            if (valueHour > hour) {
-              for (let i = hour; i < valueHour; i++) {
-                const validDate = new Date(value.readings_date);
-                const faultyHour = new Date(
-                  `${validDate.getFullYear()}-${
-                    validDate.getMonth() + 1
-                  }-${validDate.getDate()} ${i}:0`
-                );
+  switch (measurementType) {
+    case "hourly":
+      db.select(
+        "external_temperature",
+        "internal_temperature",
+        "humidity",
+        "weight",
+        "battery",
+        "readings_date"
+      )
+        .orderBy("readings_date")
+        .from("apiaries")
+        .where({
+          hive_id: hiveId,
+        })
+        .whereRaw("readings_date >= NOW() - INTERVAL '24 HOURS'")
+        .then((result) => {
+          const target = result[result.length - 1];
 
-                firstDataFromHours.push({
-                  temperature: "0",
-                  humidity: "0",
-                  weight: "0",
-                  battery: "0",
-                  readings_date: faultyHour,
-                });
-              }
-
-              hour = valueHour + 1;
-              firstDataFromHours.push(value);
-            }
-          }
-        });
-
-        db.select(
-          "external_temperature",
-          "internal_temperature",
-          "humidity",
-          "weight",
-          "battery",
-          "readings_date"
-        )
-          .from("apiaries")
-          .where({
-            hive_id: hiveId,
-          })
-          .orderBy("readings_date")
-          .then((lastValues) => {
-            const target = lastValues[lastValues.length - 1];
-
-            res.json({
-              firstDataFromHours,
-              lastValues: target,
-            });
-          })
-          .catch(() => {
-            res.json("Unable to get any data");
+          res.json({
+            firstDataFromHours: result,
+            lastValues: target,
           });
-      })
-      .catch(() => {
-        res.json(`Unable to get data`);
-      });
-  } else if (measurementType === "weekly") {
-    res.json("not available");
-  } else if (measurementType === "monthly") {
-    res.json("not available");
-  } else {
-    res.json("Invalid resource");
+        })
+        .catch(() => res.status(500).json("Something went wrong"));
+      break;
+    case "daily":
+      db.select(
+        "external_temperature",
+        "internal_temperature",
+        "humidity",
+        "weight",
+        "battery",
+        "readings_date"
+      )
+        .orderBy("readings_date")
+        .from("apiaries")
+        .where({
+          hive_id: hiveId,
+        })
+        .whereRaw("EXTRACT(DAY FROM readings_date) = ?", [day])
+        .andWhereRaw("EXTRACT(MONTH FROM readings_date) = ?", [month])
+        .andWhereRaw("EXTRACT(YEAR FROM readings_date) = ?", [year])
+        .then((data) => {
+          data.forEach((value) => {
+            const valueHour = new Date(value.readings_date).getHours();
+            if (valueHour === hour) {
+              firstDataFromHours.push(value);
+              hour++;
+            } else {
+              if (valueHour > hour) {
+                for (let i = hour; i < valueHour; i++) {
+                  const validDate = new Date(value.readings_date);
+                  const faultyHour = new Date(
+                    `${validDate.getFullYear()}-${
+                      validDate.getMonth() + 1
+                    }-${validDate.getDate()} ${i}:0`
+                  );
+
+                  firstDataFromHours.push({
+                    external_temperature: "0",
+                    internal_temperature: "0",
+                    humidity: "0",
+                    weight: "0",
+                    battery: "0",
+                    readings_date: faultyHour,
+                  });
+                }
+
+                hour = valueHour + 1;
+                firstDataFromHours.push(value);
+              }
+            }
+          });
+
+          db.select(
+            "external_temperature",
+            "internal_temperature",
+            "humidity",
+            "weight",
+            "battery",
+            "readings_date"
+          )
+            .from("apiaries")
+            .where({
+              hive_id: hiveId,
+            })
+            .orderBy("readings_date")
+            .then((lastValues) => {
+              const target = lastValues[lastValues.length - 1];
+
+              res.json({
+                firstDataFromHours,
+                lastValues: target,
+              });
+            })
+            .catch(() => {
+              res.json("Unable to get any data");
+            });
+        })
+        .catch(() => {
+          res.json(`Unable to get data`);
+        });
+      break;
+    case "weekly":
+      res.json("not available");
+      break;
+    case "monthly":
+      res.json("not available");
+      break;
+    default:
+      res.json("Invalid resource");
   }
 };
 
